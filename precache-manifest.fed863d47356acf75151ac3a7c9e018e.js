510f40a7cc6268cb24e979c34fc1d18cf0fe16fa
bik_krlvn <kelvinbirikorang@gmail.com>
2020-04-27 21:01:16 +0000

    Updates

---
+++ precache-manifest.fed863d47356acf75151ac3a7c9e018e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ae43ddb1392066dba747ed7fd23502",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "866a86dbc66ec5c87bc4",
+    "url": "/Covid-19-Tracker/static/css/2.45780812.chunk.css"
+  },
+  {
+    "revision": "519272b6a82820929223",
+    "url": "/Covid-19-Tracker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "866a86dbc66ec5c87bc4",
+    "url": "/Covid-19-Tracker/static/js/2.7c5fb197.chunk.js"
+  },
+  {
+    "revision": "fbbfe9ed4dc77af342a4a500830ec9a4",
+    "url": "/Covid-19-Tracker/static/js/2.7c5fb197.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "519272b6a82820929223",
+    "url": "/Covid-19-Tracker/static/js/main.95bd1c08.chunk.js"
+  },
+  {
+    "revision": "245714afb9e54c14f7f1",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.31781afa.js"
+  }
+]);
